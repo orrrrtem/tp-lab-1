d8bcc5b969efa31ee97eaf4ed98d906faaa0c0aa
orrrrtem <43597601+orrrrtem@users.noreply.github.com>
2018-10-20 21:45:42 +0300

    Update task1.h

--- include/task1.h
+++ include/task1.h
@@ -1,3 +1,6 @@
+#ifndef TASK1_TASK1_H
+#define TASK1_TASK1_H
+
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -5,3 +8,5 @@
 using namespace std;
 
 unsigned long findValue(unsigned int min,unsigned max);
+
+#endif
