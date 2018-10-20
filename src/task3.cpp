872953ab68c45ab0d90f8ea1f9aa085bd888d766
orrrrtem <43597601+orrrrtem@users.noreply.github.com>
2018-10-20 23:09:26 +0300

    Update task3.cpp

--- src/task3.cpp
+++ src/task3.cpp
@@ -2,12 +2,11 @@
 
 unsigned long long sumPrime(unsigned int hbound) {
     bool *fl = new bool[hbound]{false};
-    unsigned long long int s = 0;
-    unsigned int i = 2, j;
+    unsigned long long int s = 0, i = 2;
     while (i < hbound) {
         if (!fl[i]) {
             s += i;
-            for (j=i; j < (hbound); j += i)
+            for (unsigned long long int j = i; j < (hbound); j += i)
                 fl[j] = true;
         }
         i++;
