18e6dee099e790c339cca6dbdddefe23a8da1d97
Artem Aroslankin <43597601+orrrrtem@users.noreply.github.com>
2019-01-08 22:25:06 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,10 +5,16 @@
 TEST(lab4,scene1)
 {
     Automata nc;
-    nc.on();
-
+    string h = nc.on();
+    EXPECT_EQ("THE MACHINE HAS TURNED ON",h);
     
     nc.coin(10);
     int c = nc.getcash();
     EXPECT_EQ(10,c);
+    
+    nc.coin(30);
+    EXPECT_EQ(ACCEPT, nc.getstate())
+        
+    string ans = nc.choice(2);
+    EXPECT_EQ("ENJOY YOUR DRINK",ans);
 }
